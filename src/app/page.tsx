e216540b5267fdfef6d3b168b957f9315b8c9229
A-Wu5 <roboanthony@gmail.com>
2025-08-02 17:52:28 -0700

    Fixed import file mismatch for prisma client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,4 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-import { Button } from "@/components/ui/button";
-
-export default function Home() {
+export default async function Home() {
   return (
     <div className="m-4">
       <h1>homepage content</h1>
---
+++ src/lib/prisma.ts
@@ -0,0 +1,8 @@
+// Avoid creating multiple Prisma instances by using a singleton so that we only have one
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
